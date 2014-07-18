941f2406ba66a71031da4c96cb820752f5ac289d
Librecz Bernadett <librecz.bernadett@gmail.com>
2014-07-18 19:57:01 +0200

    initial commit

---
+++ src/com/namyxc/collectcheese/models/Game.java
@@ -0,0 +1,14 @@
+package com.namyxc.collectcheese.models;
+
+public class Game {
+
+	public Object player1;
+	public Object player2;
+	public Object boardDeck;
+
+	public Game(){
+		player1 = new Object();
+		player2 = new Object();
+		boardDeck = new Object();
+	}
+}
