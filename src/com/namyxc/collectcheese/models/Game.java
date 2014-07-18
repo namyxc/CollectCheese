ee70d772aa4c08af82cb14587ecdd7fe75a01503
Librecz Bernadett <librecz.bernadett@gmail.com>
2014-07-18 20:53:55 +0200

    decksizes done

--- src/com/namyxc/collectcheese/models/Game.java
+++ src/com/namyxc/collectcheese/models/Game.java
@@ -1,14 +1,22 @@
 package com.namyxc.collectcheese.models;
 
+import java.util.ArrayList;
+
 public class Game {
 
+	public static final int CARD_DECK_INITIAL_SIZE = 6;
 	public Object player1;
 	public Object player2;
-	public Object boardDeck;
+	public ArrayList<Object> boardDeck;
+	public ArrayList<Object> cardDeck;
 
 	public Game(){
 		player1 = new Object();
 		player2 = new Object();
-		boardDeck = new Object();
+		boardDeck = new ArrayList<Object>();
+		cardDeck = new ArrayList<Object>();
+		for (int i = 0; i < Game.CARD_DECK_INITIAL_SIZE;i++){
+			cardDeck.add(new Object());
+		}
 	}
 }
